90c57d3dcb77f75bba7309805c74c69b84a79bab
nuttyversal <mail@nuttyver.se>
2023-09-24 13:37:30 -0700

    Serve static directory specified by env variable

--- fonts/src/main.rs
+++ fonts/src/main.rs
@@ -1,9 +1,11 @@
+use std::env;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-	let hello =
-		warp::path!("hello" / String).map(|name| format!("Hello, {}! — Font Force Field 🛡️", name));
-
-	warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+	let font_directory = env::var("FONT_DIRECTORY").unwrap_or_else(|_| "./fonts".into());
+	let fonts = warp::fs::dir(font_directory);
+	let root = warp::path::end().map(|| format!("Font Force Field 🛡️"));
+	let routes = fonts.or(root);
+	warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
