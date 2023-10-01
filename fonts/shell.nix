850883bb7040d4fa058215c520c10fc33b37468a
nuttyversal <mail@nuttyver.se>
2023-10-01 14:57:10 -0700

    Install fonttools (for pyftsubset)
    Add pyftsubset tool to Nix shell for subsetting OpenType fonts.
    
    https://nixos.wiki/wiki/Python#Emulating_virtualenv_with_nix-shell

--- fonts/shell.nix
+++ fonts/shell.nix
@@ -4,12 +4,19 @@ in nixpkgs.stdenv.mkDerivation {
 
 	buildInputs = with nixpkgs; [
 		libiconv
+		python3
+		python3.pkgs.fonttools
 		rustup
 	];
 
 	shellHook = ''
 		export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
 		export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+
+		export PIP_PREFIX=$(pwd)/_build/pip_packages
+		export PYTHONPATH="$PIP_PREFIX/${nixpkgs.python3.sitePackages}:$PYTHONPATH"
+		export PATH="$PIP_PREFIX/bin:$PATH"
+		unset SOURCE_DATE_EPOCH
 	'';
 
 	# Used to pin the version of the Rust compiler.
