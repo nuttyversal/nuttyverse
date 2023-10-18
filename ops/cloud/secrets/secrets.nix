b1a3e82c610c60e8869d0c704b308fac1549f073
nuttyversal <mail@nuttyver.se>
2023-10-17 23:13:52 -0700

    Register SSH keys with agenix

---
+++ ops/cloud/secrets/secrets.nix
@@ -0,0 +1,6 @@
+let
+	versal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIifhdP6vTWEGGvLgWK/93bTF8TGrlffo2o/V0krRbbP mail@nuttyver.se";
+	cloud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERj1usJbWGs4w5mB3xetwWRiagZjYPc71PSg8gfjg7i root@nuttycloud";
+in {
+	# Secrets go here...
+}
