67009b8b2233eaeb23d6336aff16e4fb68f54552
nuttyversal <mail@nuttyver.se>
2023-10-20 17:33:12 -0700

    Update image name for Font Force Field 🛡️
    Since I am using Gitea as a Docker registry now!

--- ops/cloud/containers/font-force-field.nix
+++ ops/cloud/containers/font-force-field.nix
@@ -5,7 +5,7 @@
 		oci-containers = {
 			containers = {
 				font-force-field = {
-					image = "localhost/nuttyverse/font-force-field";
+					image = "code.nuttyver.se/observable/font-force-field:latest";
 
 					ports = [
 						"3668:3030"
