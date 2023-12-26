f78963a893244ffcdf446658ce73325cd541cdbd
nuttyversal <mail@nuttyver.se>
2023-12-26 16:10:43 -0700

    Add default values for variable font props

--- blocks/src/components/Text/Text.tsx
+++ blocks/src/components/Text/Text.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Text.css'
 import { TextVariants, text } from './Text.css';
 
+const DEFAULT_OPSZ = 18;
+const DEFAULT_WDTH = 85;
+const DEFAULT_WEIGHT = 400;
+
 export function createFontVariationSettings({ opsz, wdth }: { opsz: number, wdth: number }) {
 	return `"opsz" ${opsz}, "wdth" ${wdth}`;
 }
@@ -9,16 +13,26 @@ export function createFontVariationSettings({ opsz, wdth }: { opsz: number, wdth
 type TextProps = {
 	children: React.ReactNode;
 	size?: NonNullable<TextVariants>['size'];
-	opsz: number;
-	wdth: number;
-	weight: number;
+	opsz?: number;
+	wdth?: number;
+	weight?: number;
 };
 
 export const Text: React.FC<TextProps> = (props) => {
-	const fontVariationSettings = createFontVariationSettings(props);
-	const transition = 'all 0.2s ease-out';
-	const fontWeight = props.weight;
-	const className = text({ size: props.size });
+	const fontVariationSettings = createFontVariationSettings({
+		opsz: props.opsz ?? DEFAULT_OPSZ,
+		wdth: props.wdth ?? DEFAULT_WDTH,
+	});
 
-	return <div style={{ fontVariationSettings, fontWeight, transition }} className={className}>{props.children}</div>;
+	const fontWeight = props.weight ?? DEFAULT_WEIGHT;
+	const transition = 'all 0.2s ease-out';
+
+	return (
+		<div
+			style={{ fontVariationSettings, fontWeight, transition }}
+			className={text({ size: props.size })}
+		>
+			{props.children}
+		</div>
+	);
 };
