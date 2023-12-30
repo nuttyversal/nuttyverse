f519fc847f3b684ba13f325a3cb00b525fff0a63
nuttyversal <mail@nuttyver.se>
2023-12-29 21:24:56 -0700

    Add metasyntactical horcruxes for all font faces

--- fonts/scripts/subset-fonts.fish
+++ fonts/scripts/subset-fonts.fish
@@ -9,3 +9,59 @@ pyftsubset \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProR_liga_0830.woff2 \
+	--output-file=pragmatapro-liga-regular-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProB_liga_0830.woff2 \
+	--output-file=pragmatapro-liga-bold-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProI_liga_0830.woff2 \
+	--output-file=pragmatapro-liga-italic-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProZ_liga_0830.woff2 \
+	--output-file=pragmatapro-liga-bold-italic-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProFraktur.woff2 \
+	--output-file=pragmatapro-fraktur-regular-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/PragmataProFrakturB.woff2 \
+	--output-file=pragmatapro-fraktur-bold-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes="$basic_latin"
+
+pyftsubset \
+	target/fsd_emoji_1.0-webfont.woff2 \
+	--output-file=fsd-emoji-foo.woff2 \
+	--flavor=woff2 \
+	--with-zopfli \
+	--layout-features="" \
+	--unicodes=""
