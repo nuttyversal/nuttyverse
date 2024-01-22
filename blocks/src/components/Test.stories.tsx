181107b702e55da452890e41323103de2ff4721f
nuttyversal <mail@nuttyver.se>
2024-01-21 23:53:19 -0700

    Preview landing page redesign

---
+++ blocks/src/components/Test.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Test } from './Test';
+
+const meta: Meta<typeof Test> = {
+	component: Test,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+export const Component: Story = {};
