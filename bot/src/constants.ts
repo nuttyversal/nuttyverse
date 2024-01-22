3aa89c826dc3179adf10563e496839e2dcd3ba19
nuttyversal <mail@nuttyver.se>
2024-01-21 22:43:02 -0700

    Respond to mentions in #linkshell

--- bot/src/constants.ts
+++ bot/src/constants.ts
@@ -2,3 +2,9 @@ export const channelIdByName = {
 	linkshell: '1186832865661550665',
 	botTesting: '1198853726153232409',
 };
+
+export const userIdByName = {
+	nuttyversal: '1127428266848956446',
+	nuttybot: '1198835079586271233',
+	PrinceWalnut: '191021670088048653',
+}
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,6 +1,6 @@
 import { Client, Events, GatewayIntentBits, TextChannel } from 'discord.js';
 import dotenv from 'dotenv';
-import { channelIdByName } from './constants.js';
+import { channelIdByName, userIdByName } from './constants.js';
 
 async function start() {
 	const client = new Client({
@@ -24,6 +24,24 @@ async function start() {
 
 		// Reply to the message with the same content.
 		message.channel.send(message.content);
+	});
+
+	client.on(Events.MessageCreate, (message) => {
+		// Ignore messages from bots.
+		if (message.author.bot) return;
+
+		// Only reply to messages in #linkshell.
+		if (message.channelId !== channelIdByName.linkshell) return;
+
+		if (message.mentions.has(userIdByName.nuttybot)) {
+			if (message.author.id === userIdByName.nuttyversal) {
+				message.channel.send('Hello! ٩(◕‿◕｡)۶');
+			} else if (message.author.id === userIdByName.PrinceWalnut) {
+				message.channel.send('Fuck you! (¬_¬")');
+			} else {
+				message.channel.send('Hiya! (⸝⸝ᵕᴗᵕ⸝⸝)');
+			}
+		}
 	});
 
 	await client.login(process.env.DISCORD_BOT_TOKEN);
