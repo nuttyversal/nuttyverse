ce647c966196e6976564421bacb72bd9c432d3c7
nuttyversal <mail@nuttyver.se>
2024-01-21 22:17:09 -0700

    Echo every message in #bot-testing channel

---
+++ bot/src/constants.ts
@@ -0,0 +1,4 @@
+export const channelIdByName = {
+	linkshell: '1186832865661550665',
+	botTesting: '1198853726153232409',
+};
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,14 +1,30 @@
-import { Client, Events } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
+import { channelIdByName } from './constants.js';
 
 dotenv.config();
 
 const client = new Client({
-	intents: [],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
 client.once(Events.ClientReady, (client) => {
 	console.log(`Ready! Logged in as ${client.user.tag}.`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+	// Ignore messages from bots.
+	if (message.author.bot) return;
+
+	// Only reply to messages in #bot-testing.
+	if (message.channelId !== channelIdByName.botTesting) return;
+
+	// Reply to the message with the same content.
+	message.channel.send(message.content);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
