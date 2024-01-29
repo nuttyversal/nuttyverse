263dbea3e8ededd7ccf86dc2b41cae0625113aa6
nuttyversal <mail@nuttyver.se>
2024-01-28 22:44:12 -0700

    Clean up formatted text in JSX markup

--- blocks/src/components/Test.tsx
+++ blocks/src/components/Test.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { Text } from "./Text/Text";
 
 export const Test = () => {
+	const physical = (
+		<Text as="span" wdth={115} weight={350} style={{ color: "#3E8857" }}>
+			physical
+		</Text>
+	);
+
+	const digital = (
+		<Text as="span" wdth={115} weight={350} style={{ color: "#7F7CED" }}>
+			digital
+		</Text>
+	);
+
 	return (
 		<section style={{ width: "425px" }}>
 			<Text dropCap>
@@ -11,32 +23,9 @@ export const Test = () => {
 			</Text>
 
 			<Text>
-				Just as the{" "}
-				<Text
-					as="span"
-					wdth={115}
-					weight={350}
-					style={{
-						color: "#3E8857",
-					}}
-				>
-					{" "}
-					physical
-				</Text>{" "}
-				universe materialized from the Big Bang, this website will similarly
-				unfold from this point. Emerging from this singularity will bloom a
-				new{" "}
-				<Text
-					as="span"
-					wdth={115}
-					weight={350}
-					style={{
-						color: "#7F7CED",
-					}}
-				>
-					digital
-				</Text>{" "}
-				universe.
+				Just as the {physical} universe materialized from the Big Bang, this
+				website will similarly unfold from this point. Emerging from this
+				singularity will bloom a new {digital} universe.
 			</Text>
 		</section>
 	);
