63489a415874e7821444962d773e93b981cb5819
nuttyversal <mail@nuttyver.se>
2024-01-30 18:34:43 -0700

    Connect local packages with pnpm monorepo setup
    ✦ Update package TS config to extend hoisted root config.
    ✦ Install React integration for Astro.
    ✦ Add vanilla-extract Vite plugin in @nuttyverse/city.
    ✦ Add @nuttyverse/blocks package dependency in @nuttyverse/city.

---
+++ city/src/components/Introduction.tsx
@@ -0,0 +1,31 @@
+import { Text } from "@nuttyverse/blocks";
+
+export const Introduction = () => {
+	const physical = (
+		<Text as="span" wdth={115} weight={350} style={{ color: "#3E8857" }}>
+			physical
+		</Text>
+	);
+
+	const digital = (
+		<Text as="span" wdth={115} weight={350} style={{ color: "#7F7CED" }}>
+			digital
+		</Text>
+	);
+
+	return (
+		<section style={{ width: "425px" }}>
+			<Text dropCap>
+				Ahoy there, fellow navigator! Welcome to my internet expanse. You
+				might notice a seeming emptiness, but do not fret. You've simply
+				arrived early enough to witness the expansion of this universe.
+			</Text>
+
+			<Text>
+				Just as the {physical} universe materialized from the Big Bang, this
+				website will similarly unfold from this point. Emerging from this
+				singularity will bloom a new {digital} universe.
+			</Text>
+		</section>
+	);
+};
