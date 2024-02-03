a81343733f1fa0f8600fcba894549768254f81eb
nuttyversal <mail@nuttyver.se>
2024-02-02 18:01:05 -0700

    Point font subsetting script to unobservable files
    ✦ Prepare directory for storing metasyntactical horcruxes.
    ✦ Add output logging so I can see what's happening.

--- fonts/scripts/subset-fonts.fish
+++ fonts/scripts/subset-fonts.fish
@@ -1,66 +1,83 @@
 #!/usr/bin/env fish
 
+set nure_dir_path "../unobservable/fonts/Nure1.2W/Variable font compressed"
+set pragmata_dir_path "../unobservable/fonts/PragmataPro0.830W"
+set pragmata_fraktur_dir_path "../unobservable/fonts/PragmataProFraktur1.2W"
+set fsd_emoji_dir_path "../unobservable/fonts/FSD Emoji1.0W"
+
 set basic_latin "U+0020-007E"
 
+if not test -d horcruxes
+	mkdir horcruxes
+end
+
+echo "Creating metasyntactical horcrux: nure-foo.woff2"
 pyftsubset \
-	target/Nure12-VF.woff2 \
-	--output-file=nure-foo.woff2 \
+	$nure_dir_path/Nure12-VF.woff2 \
+	--output-file=horcruxes/nure-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-foo.woff2"
 pyftsubset \
-	target/PragmataProR_liga_0830.woff2 \
-	--output-file=pragmatapro-liga-regular-foo.woff2 \
+	$pragmata_dir_path/PragmataProR_liga_0830.woff2 \
+	--output-file=horcruxes/pragmatapro-liga-regular-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-liga-bold-foo.woff2"
 pyftsubset \
-	target/PragmataProB_liga_0830.woff2 \
-	--output-file=pragmatapro-liga-bold-foo.woff2 \
+	$pragmata_dir_path/PragmataProB_liga_0830.woff2 \
+	--output-file=horcruxes/pragmatapro-liga-bold-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-liga-italic-foo.woff2"
 pyftsubset \
-	target/PragmataProI_liga_0830.woff2 \
-	--output-file=pragmatapro-liga-italic-foo.woff2 \
+	$pragmata_dir_path/PragmataProI_liga_0830.woff2 \
+	--output-file=horcruxes/pragmatapro-liga-italic-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-liga-bold-italic-foo.woff2"
 pyftsubset \
-	target/PragmataProZ_liga_0830.woff2 \
-	--output-file=pragmatapro-liga-bold-italic-foo.woff2 \
+	$pragmata_dir_path/PragmataProZ_liga_0830.woff2 \
+	--output-file=horcruxes/pragmatapro-liga-bold-italic-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-regular-foo.woff2"
 pyftsubset \
-	target/PragmataProFraktur.woff2 \
-	--output-file=pragmatapro-fraktur-regular-foo.woff2 \
+	$pragmata_fraktur_dir_path/PragmataProFraktur.woff2 \
+	--output-file=horcruxes/pragmatapro-fraktur-regular-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: pragmatapro-bold-foo.woff2"
 pyftsubset \
-	target/PragmataProFrakturB.woff2 \
-	--output-file=pragmatapro-fraktur-bold-foo.woff2 \
+	$pragmata_fraktur_dir_path/PragmataProFrakturB.woff2 \
+	--output-file=horcruxes/pragmatapro-fraktur-bold-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
 	--unicodes="$basic_latin"
 
+echo "Creating metasyntactical horcrux: fsd-emoji-foo.woff2"
 pyftsubset \
-	target/fsd_emoji_1.0-webfont.woff2 \
-	--output-file=fsd-emoji-foo.woff2 \
+	$fsd_emoji_dir_path/fsd_emoji_1.0-webfont.woff2 \
+	--output-file=horcruxes/fsd-emoji-foo.woff2 \
 	--flavor=woff2 \
 	--with-zopfli \
 	--layout-features="" \
