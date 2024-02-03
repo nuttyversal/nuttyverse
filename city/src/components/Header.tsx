6ce2896d21004563f812ce6a49a4b5077c921864
nuttyversal <mail@nuttyver.se>
2024-02-02 23:53:07 -0700

    Add Nutty chibi to header

---
+++ city/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { Text } from "@nuttyverse/blocks";
+
+export const Header = () => {
+	return (
+		<div>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "space-between",
+					alignItems: "end",
+				}}
+			>
+				<Text as="h1" style={{ margin: 0 }} wdth={90} weight={700}>
+					Nuttyverse
+				</Text>
+
+				<img
+					src="/nuttyversal.svg"
+					alt="Nuttyverse"
+					style={{
+						width: "140px",
+						paddingRight: "0.5em",
+
+						// Make flush with bottom border.
+						marginBottom: "-1px",
+						zIndex: -1,
+					}}
+				/>
+			</div>
+
+			<div
+				style={{
+					height: "3px",
+					width: "calc(100% + 32px)",
+					marginLeft: "-16px",
+					background: "black",
+					marginBottom: "1.5em",
+				}}
+			/>
+		</div>
+	);
+};
