e81410fb16f53d2bcf66c79fa08febaf4c1c1790
nuttyversal <mail@nuttyver.se>
2024-02-19 00:44:12 -0700

    Add component prop for opening links in new tabs

--- blocks/src/atoms/Link/Link.tsx
+++ blocks/src/atoms/Link/Link.tsx
@@ -1,14 +1,21 @@
+import React from "react";
 import { Text } from "../Text/Text";
 import { link } from "./Link.css";
 
 type Props = {
 	children: React.ReactNode;
 	href: string;
+	newTab?: boolean;
 };
 
 export const Link: React.FC<Props> = (props) => {
 	return (
-		<Text as="a" className={link} href={props.href}>
+		<Text
+			as="a"
+			className={link}
+			href={props.href}
+			target={props.newTab ? "_blank" : ""}
+		>
 			{props.children}
 		</Text>
 	);
