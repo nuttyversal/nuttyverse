4c9b3853737ecd28c3bf67a5e9d90c51dac35b14
nuttyversal <mail@nuttyver.se>
2024-02-19 12:05:16 -0700

    Prefer `all-small-caps` over text transform

--- blocks/src/atoms/Link/Link.css.ts
+++ blocks/src/atoms/Link/Link.css.ts
@@ -2,9 +2,8 @@ import { style } from "@vanilla-extract/css";
 
 export const link = style({
 	fontFeatureSettings: `"smcp" 1, "c2sc" 1`,
-	fontVariant: "small-caps",
-	fontVariantCaps: "small-caps",
-	textTransform: "lowercase",
+	fontVariant: "all-small-caps",
+	fontVariantCaps: "all-small-caps",
 	textUnderlineOffset: "3px",
 	fontFamily: "Nure",
 	textDecoration: "none",
