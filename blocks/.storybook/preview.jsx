32df1375391ae804b8f20264df4fa0503654f93b
nuttyversal <mail@nuttyver.se>
2024-02-24 14:19:00 -0700

    Add theme toggle button to Storybook decorator

--- blocks/.storybook/preview.jsx
+++ blocks/.storybook/preview.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { NuttyverseContext } from "../src/styles/themes/Context";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
 	parameters: {
@@ -10,19 +13,45 @@ const preview = {
 		},
 	},
 	decorators: [
-		(Story) => (
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					width: "100%",
-				}}
-			>
-				<div style={{ width: "30em" }}>
-					<Story />
-				</div>
-			</div>
-		),
+		(Story) => {
+			const [theme, setTheme] = useState("light");
+			const themeButtonText = theme === "light" ? "☀️" : "🌙";
+			const backgroundColor = theme === "light" ? "white" : "black";
+
+			const toggleTheme = () => {
+				setTheme((prevTheme) => {
+					return prevTheme === "light" ? "dark" : "light";
+				});
+			};
+
+			return (
+				<>
+					<div
+						style={{
+							position: "absolute",
+							top: "1em",
+							right: "1em",
+						}}
+					>
+						<button onClick={toggleTheme}>{themeButtonText}</button>
+					</div>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							width: "100%",
+							backgroundColor,
+						}}
+					>
+						<div style={{ width: "30em", padding: "1em" }}>
+							<NuttyverseContext.Provider value={theme}>
+								<Story />
+							</NuttyverseContext.Provider>
+						</div>
+					</div>
+				</>
+			);
+		},
 	],
 };
 
