9f84c711d057871d7a6971bf1f8994fd81caeedf
nuttyversal <mail@nuttyver.se>
2024-02-24 17:50:28 -0700

    Detect color scheme on component mount

--- city/src/components/Landing.tsx
+++ city/src/components/Landing.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { NuttyverseContext, Singularity } from "@nuttyverse/blocks";
 
 export const Landing = () => {
-	const [theme, setTheme] = useState<"light" | "dark">("light");
-
 	const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+	const defaultTheme = colorSchemeQuery.matches ? "dark" : "light";
+	const [theme, setTheme] = useState<"light" | "dark">(defaultTheme);
 
 	colorSchemeQuery.addEventListener("change", (event) => {
 		setTheme(event.matches ? "dark" : "light");
