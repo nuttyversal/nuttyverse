f0c18d63aa2cdc919287464a99fcb7bf36e6939d
nuttyversal <mail@nuttyver.se>
2024-02-24 23:22:32 -0700

    Make image glow in the dark

--- blocks/src/atoms/Image/Image.css.ts
+++ blocks/src/atoms/Image/Image.css.ts
@@ -2,14 +2,23 @@ import { createTheme, createThemeContract, style } from "@vanilla-extract/css";
 
 export const vars = createThemeContract({
 	borderColor: null,
+	glow: {
+		boxShadow: null,
+	},
 });
 
 export const lightMode = createTheme(vars, {
 	borderColor: "black",
+	glow: {
+		boxShadow: "none",
+	},
 });
 
 export const darkMode = createTheme(vars, {
 	borderColor: "white",
+	glow: {
+		boxShadow: "0 0 10px 5px rgba(255, 255, 255, 0.5)",
+	},
 });
 
 export const base = style({
@@ -18,4 +27,8 @@ export const base = style({
 	borderRadius: "3px",
 	borderStyle: "solid",
 	imageRendering: "pixelated",
+});
+
+export const withGlow = style({
+	boxShadow: vars.glow.boxShadow,
 });
