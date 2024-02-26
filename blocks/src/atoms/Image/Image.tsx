5ade0183fdff7a14bbbc6968613169e0997d56d4
nuttyversal <mail@nuttyver.se>
2024-02-25 19:25:42 -0700

    Add glow prop to Image component

--- blocks/src/atoms/Image/Image.tsx
+++ blocks/src/atoms/Image/Image.tsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
-import { base, darkMode, lightMode } from "./Image.css";
+import { base, darkMode, lightMode, withGlow } from "./Image.css";
 import { NuttyverseContext } from "../../styles/themes/Context";
 
-type ImageProps = {} & React.ComponentPropsWithoutRef<"img">;
+type ImageProps = {
+	glow?: boolean | any;
+} & React.ComponentPropsWithoutRef<"img">;
 
 export const Image = (props: ImageProps) => {
+	const { glow, ...imgProps } = props;
 	const theme = useContext(NuttyverseContext);
 	const themeClass = theme === "light" ? lightMode : darkMode;
+	const classNames = [base, themeClass, glow ? withGlow : undefined];
 
-	return <img className={[base, themeClass].join(" ")} {...props} />;
+	return <img className={classNames.join(" ")} {...imgProps} />;
 };
