04012ba7c784d4fce4e229e650304f8a9a456032
nuttyversal <mail@nuttyver.se>
2024-03-05 21:30:10 -0700

    Change ownership of Vaultwarden environment secret

--- ops/cloud/services/vaultwarden.nix
+++ ops/cloud/services/vaultwarden.nix
@@ -40,8 +40,8 @@
 			vaultwarden-environment = {
 				file = ../secrets/vaultwarden-environment.age;
 				path = "/run/secrets/vaultwarden-environment";
-				owner = "root";
-				group = "root";
+				owner = "vaultwarden";
+				group = "vaultwarden";
 				mode = "600";
 			};
 		};
