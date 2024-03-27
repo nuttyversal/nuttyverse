77408d1f417e2c45f5e493b1070461bf46ebd8a4
nuttyversal <mail@nuttyver.se>
2024-03-27 14:46:24 -0700

    Replace Nix shell with Nix flake

---
+++ bot/flake.nix
@@ -0,0 +1,30 @@
+{
+   description = "Nuttyverse Bot";
+
+   inputs = {
+      nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+   };
+
+   outputs = { self, flake-utils, nixpkgs }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in {
+				devShells.default = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						just
+						nodePackages.pnpm
+						nodejs
+					];
+				};
+			}
+		);
+}
