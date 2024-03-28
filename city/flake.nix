e0cd9c2d4bdaf2f00651addf2c621be2d2a769bb
nuttyversal <mail@nuttyver.se>
2024-03-27 17:22:18 -0700

    Replace Nix shell with Nix flake

---
+++ city/flake.nix
@@ -0,0 +1,36 @@
+{
+   description = "Nuttyverse City";
+
+   inputs = {
+      nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+   };
+
+   outputs = { self, flake-utils, nixpkgs }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in {
+				devShells.default = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						just
+						nodePackages.pnpm
+						ruby
+					];
+
+					shellHook = ''
+						export GEM_HOME="$PWD/.gem"
+						export PATH="$GEM_HOME/bin:$PATH"
+						${pkgs.ruby}/bin/gem install --no-user-install --install-dir $GEM_HOME neocities
+					'';
+				};
+			}
+		);
+}
