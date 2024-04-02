090b81e664626d7b32be112742fd97041b0361a2
nuttyversal <mail@nuttyver.se>
2024-04-02 16:30:59 -0700

    Prefer absolute path over relative path

--- tools/fish/config.fish
+++ tools/fish/config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
-	source ./config/abbr.fish
-	source ./config/bat.fish
-	source ./config/eza.fish
-	source ./config/fzf.fish
-	source ./config/gpg.fish
-	source ./config/prompt.fish
-	source ./config/vim.fish
-	source ./config/warp.fish
+	set config_dir (dirname (status -f))
+	source $config_dir/config/abbr.fish
+	source $config_dir/config/bat.fish
+	source $config_dir/config/eza.fish
+	source $config_dir/config/fzf.fish
+	source $config_dir/config/gpg.fish
+	source $config_dir/config/greeting.fish
+	source $config_dir/config/prompt.fish
+	source $config_dir/config/vim.fish
+	source $config_dir/config/warp.fish
 end
