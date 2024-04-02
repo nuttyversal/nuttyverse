19923a570ec7a54584edbe91818af27e25765d0d
nuttyversal <mail@nuttyver.se>
2024-04-02 15:36:33 -0700

    Configure fish config entry point

---
+++ tools/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+	source ./config/abbr.fish
+	source ./config/bat.fish
+	source ./config/eza.fish
+	source ./config/fzf.fish
+	source ./config/gpg.fish
+	source ./config/prompt.fish
+	source ./config/vim.fish
+	source ./config/warp.fish
+end
