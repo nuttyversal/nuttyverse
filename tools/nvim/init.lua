2c59491526ad1f78496811ca1b832384533e8c2c
nuttyversal <mail@nuttyver.se>
2024-04-03 10:57:49 -0700

    Load plugin manager at the end of `init.lua`
    Need to make sure to set `mapleader` before lazy to ensure that mappings
    are correct.

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -1,23 +1,4 @@
 --
--- Plugin manager
---
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(plugins, opts)
-
---
 -- Persistence
 --
 
@@ -128,3 +109,23 @@ vim.api.nvim_set_keymap('n', '//', ':noh<CR>', { noremap = true })
 
 -- Save file
 vim.api.nvim_set_keymap('n', '<LEADER>w', ':w<CR>', {noremap = true, silent = true})
+
+--
+-- Plugin manager
+--
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
