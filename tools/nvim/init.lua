44edd7de9b9ba128eac4c9352c54147477280058
nuttyversal <mail@nuttyver.se>
2024-04-04 09:27:49 -0700

    Enable copy-pasta with macOS bindings in Neovide

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -91,6 +91,13 @@ if vim.g.neovide then
 
 	vim.o.guifont = "PragmataPro Liga:h18"
 	vim.o.linespace = 2
+
+   -- Allow clipboard copy-pasta.
+   vim.g.neovide_input_use_logo = 1
+   vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+   vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+   vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+   vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 end
 
 --
