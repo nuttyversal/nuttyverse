be3f89202503098fbd6a7380c59581c6a4c750a9
nuttyversal <mail@nuttyver.se>
2024-04-03 22:13:22 -0700

    Revert to non-monospaced font face

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -87,7 +87,7 @@ if vim.g.neovide then
 	vim.g.neovide_refresh_rate = 144
 	vim.g.neovide_scale_factor = 1
 
-	vim.o.guifont = "PragmataPro Mono Liga:h18"
+	vim.o.guifont = "PragmataPro Liga:h18"
 	vim.o.linespace = 2
 end
 
