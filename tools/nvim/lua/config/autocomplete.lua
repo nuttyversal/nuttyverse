bcd057b36bba555e4b7628d6a3665a4aa67719b7
nuttyversal <mail@nuttyver.se>
2024-04-04 13:43:30 -0700

    Configure window borders for nvim-cmp

--- tools/nvim/lua/config/autocomplete.lua
+++ tools/nvim/lua/config/autocomplete.lua
@@ -12,6 +12,11 @@ cmp.setup {
 
 	completion = { completeopt = 'menu,menuone,noinsert' },
 
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+
 	-- For an understanding of why these mappings were
 	-- chosen, you will need to read `:help ins-completion`
 	--
