2769f96db9cfc5a60a7b2d87743f5b9460a62c68
nuttyversal <mail@nuttyver.se>
2024-04-04 10:04:34 -0700

    Use rounder separator glyphs

--- tools/nvim/lua/config/lualine.lua
+++ tools/nvim/lua/config/lualine.lua
@@ -1,5 +1,7 @@
 require('lualine').setup({
 	options = {
 		icons_enabled = false,
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' }
 	}
 })
