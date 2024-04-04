9ab115d5bc5117bb37e16d414213cf9531d6d6b7
nuttyversal <mail@nuttyver.se>
2024-04-04 13:13:23 -0700

    Add mappings for tab completion

--- tools/nvim/lua/config/autocomplete.lua
+++ tools/nvim/lua/config/autocomplete.lua
@@ -56,6 +56,31 @@ cmp.setup {
 			end
 		end, { 'i', 's' }),
 
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif require("luasnip").expand_or_jumpable() then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif require("luasnip").jumpable(-1) then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
+
 		-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 		--		https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 	},
