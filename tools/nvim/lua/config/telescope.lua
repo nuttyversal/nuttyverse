dfe27e6446f7505cc3593a30fd8800a14bce70e3
nuttyversal <mail@nuttyver.se>
2024-04-04 14:51:17 -0700

    Disable devicons in telescope
    Because they have a double cell width with the non-monospaced version of
    PragmataPro Liga and I don't see a simple way to update the formatting
    for the picker.

--- tools/nvim/lua/config/telescope.lua
+++ tools/nvim/lua/config/telescope.lua
@@ -3,3 +3,11 @@ vim.keymap.set('n', '<leader><space>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup {
+	pickers = {
+		find_files = {
+			disable_devicons = true
+		}
+	}
+}
