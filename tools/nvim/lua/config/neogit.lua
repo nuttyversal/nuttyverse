403993ad5cbcc3f8f51be813ff51e73a6a51f41e
nuttyversal <mail@nuttyver.se>
2024-04-17 15:36:22 -0700

    Install neogit plugin
    I was quite fond of magit when I was exposed to it via Doom Emacs a few
    years ago. This Neovim port seems promising as a replacement.

---
+++ tools/nvim/lua/config/neogit.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_set_keymap('n', '<LEADER>gg', ':Neogit<CR>', { noremap = true })
+
+require("neogit").setup {
+	graph_style = "unicode",
+
+	signs = {
+		-- { CLOSED, OPENED }
+		hunk = { "", "" },
+		item = { " ", " " },
+		section = { " ", " " },
+	},
+
+	integrations = {
+		telescope = true,
+		diffview = true,
+	},
+}
