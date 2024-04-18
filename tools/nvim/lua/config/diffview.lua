b65a926ab7f4051e438995ee7922b58f57c61e7b
nuttyversal <mail@nuttyver.se>
2024-04-17 22:15:29 -0700

    Add mapping to close diffview

--- tools/nvim/lua/config/diffview.lua
+++ tools/nvim/lua/config/diffview.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_keymap("n", "<LEADER>dd", ":DiffviewOpen<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<LEADER>dc", ":DiffviewClose<CR>", { noremap = true })
 
 require("diffview").setup({
 	use_icons = false,
