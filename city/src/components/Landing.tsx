5895cbd70023ef7908bf1d2d59f131128528413f
nuttyversal <mail@nuttyver.se>
2024-04-24 14:09:10 -0700

    Create base layout
    ✦ Create base layout in Astro.
    ✦ Update landing page to use base layout.
    ✦ Abstract theme logic into React provider component.

--- city/src/components/Landing.tsx
+++ city/src/components/Landing.tsx
@@ -1,43 +1,10 @@
-import { useState } from "react";
-import {
-	NuttyverseContext,
-	Singularity,
-	setDocumentRootBackground,
-} from "@nuttyverse/blocks";
-import { body, darkMode, lightMode, main } from "./Landing.css.ts";
+import { Singularity } from "@nuttyverse/blocks";
+import { ThemeProvider } from "./ThemeProvider.tsx";
 
 export const Landing = () => {
-	const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-	const defaultTheme = colorSchemeQuery.matches ? "dark" : "light";
-
-	const [theme, setTheme] = useState<"light" | "dark">(defaultTheme);
-	setDocumentRootBackground(defaultTheme);
-
-	colorSchemeQuery.addEventListener("change", (event) => {
-		setTheme(event.matches ? "dark" : "light");
-		setDocumentRootBackground(event.matches ? "dark" : "light");
-	});
-
-	const toggleTheme = () => {
-		setTheme(theme === "light" ? "dark" : "light");
-		setDocumentRootBackground(theme === "light" ? "dark" : "light");
-	};
-
-	const initialContext = {
-		theme,
-		setTheme,
-		toggleTheme,
-	} as const;
-
-	const themeClass = theme === "light" ? lightMode : darkMode;
-
 	return (
-		<NuttyverseContext.Provider value={initialContext}>
-			<div className={[body, themeClass].join(" ")}>
-				<main className={main}>
-					<Singularity />
-				</main>
-			</div>
-		</NuttyverseContext.Provider>
+		<ThemeProvider>
+			<Singularity />
+		</ThemeProvider>
 	);
 };
