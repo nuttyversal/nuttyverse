1e1d5fc1aa0e76b5f6b5ca35bab63b50547d5186
nuttyversal <mail@nuttyver.se>
2024-04-24 00:51:31 -0700

    Update Header to use MessageOfTheDay component

--- blocks/src/molecules/Header/Header.tsx
+++ blocks/src/molecules/Header/Header.tsx
@@ -1,39 +1,13 @@
 import { useContext } from "react";
 import { NuttyverseContext } from "~/styles/themes/context";
 import { spacing } from "~/styles/tokens/spacing";
-import { Logo } from "~/atoms/Logo/Logo";
-import { Marquee } from "~/atoms/Marquee/Marquee";
-import { Text } from "~/atoms/Text/Text";
-import { ChibiButton } from "~/molecules/ChibiButton/ChibiButton";
-import {
-	chibi,
-	container,
-	darkMode,
-	marqueeBox,
-	lightMode,
-} from "./Header.css";
+import { Logo } from "~/atoms/Logo";
+import { ChibiButton } from "~/molecules/ChibiButton";
+import { MessageOfTheDay } from "~/molecules/MessageOfTheDay";
+import { chibi, container } from "./Header.css";
 
 export const Header = () => {
 	const context = useContext(NuttyverseContext);
-	const themeClass = context.theme === "light" ? lightMode : darkMode;
-
-	const messageOfTheDay = (
-		<div className={[themeClass, marqueeBox].join(" ")}>
-			<Marquee>
-				<Text
-					size="smol"
-					weight={600}
-					style={{
-						lineHeight: "0",
-						margin: "1em",
-						fontFamily: "PragmataPro Liga",
-					}}
-				>
-					Hello there, welcome to the Nuttyverse!
-				</Text>
-			</Marquee>
-		</div>
-	);
 
 	return (
 		<div>
@@ -42,7 +16,7 @@ export const Header = () => {
 				<ChibiButton className={chibi} onClick={context.toggleTheme} />
 			</div>
 
-			{messageOfTheDay}
+			<MessageOfTheDay />
 		</div>
 	);
 };
