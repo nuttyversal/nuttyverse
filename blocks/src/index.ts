1e8a97ca6ce9af6ae1c00bbdc3d25426f46ea6d8
nuttyversal <mail@nuttyver.se>
2024-04-24 00:14:03 -0700

    Organize package exports with barrel files

--- blocks/src/index.ts
+++ blocks/src/index.ts
@@ -1,18 +1,53 @@
 // Import CSS reset as a side effect.
 import "~/styles/reset.css";
 
-import { Button } from "~/atoms/Button/Button";
-import { Text } from "~/atoms/Text/Text";
+import * as colors from "~/styles/tokens/colors";
+import { spacing } from "~/styles/tokens/spacing";
+import { experimentalTypeScale } from "~/styles/tokens/typography";
+
+import { Button } from "~/atoms/Button";
+import { Chibi } from "~/atoms/Chibi";
+import { Image } from "~/atoms/Image";
+import { ListItem, OrderedList, UnorderedList } from "~/atoms/List";
+import { Logo } from "~/atoms/Logo";
+import { Marquee } from "~/atoms/Marquee";
+import { Text } from "~/atoms/Text";
+
+import { ChibiButton } from "~/molecules/ChibiButton";
+import { Header } from "~/molecules/Header";
+import { Introduction } from "~/molecules/Introduction";
+
 import { Singularity } from "~/organisms/Singularity/Singularity";
+
 import { NuttyverseContext } from "~/styles/themes/context";
 import { setDocumentRootBackground } from "~/styles/themes/utils";
-import * as colors from "~/styles/tokens/colors";
 
 export {
+	// Tokens
+	colors,
+	spacing,
+	experimentalTypeScale,
+
+	// Atoms
 	Button,
+	Chibi,
+	Image,
+	ListItem,
+	OrderedList,
+	UnorderedList,
+	Logo,
+	Marquee,
 	Text,
+
+	// Molecules
+	ChibiButton,
+	Header,
+	Introduction,
+
+	// Organisms
 	Singularity,
+
+	// Utilities
 	NuttyverseContext,
 	setDocumentRootBackground,
-	colors,
 };
