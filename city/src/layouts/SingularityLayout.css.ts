fc18272b4e5ca63e45bd6d8e78f1ce2d4248de07
nuttyversal <mail@nuttyver.se>
2024-05-01 11:50:35 -0700

    Create `SingularityLayout` for splash page

---
+++ city/src/layouts/SingularityLayout.css.ts
@@ -0,0 +1,23 @@
+import { style } from "@vanilla-extract/css";
+import { colors, spacing } from "@nuttyverse/blocks";
+
+export const body = style({
+	backgroundColor: colors.background,
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	margin: "auto",
+	width: spacing[144],
+	height: "100dvh",
+
+	"@media": {
+		[`screen and (max-width: ${spacing[144]})`]: {
+			width: "100%",
+		},
+	},
+});
+
+export const main = style({
+	width: "100%",
+});
