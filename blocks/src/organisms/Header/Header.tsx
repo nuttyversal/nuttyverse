379d1ebe8b629d5db17bf95e46d725cc0d575f5a
nuttyversal <mail@nuttyver.se>
2024-05-01 15:40:10 -0700

    Add view transitions to page layouts

--- blocks/src/organisms/Header/Header.tsx
+++ blocks/src/organisms/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useThemeSwitcher } from "~/styles/themes/contract";
 import { ChibiButton } from "~/molecules/ChibiButton";
 import { LogoButton } from "~/molecules/LogoButton";
@@ -5,16 +6,29 @@ import { MessageOfTheDay } from "~/molecules/MessageOfTheDay";
 import { chibi, container, header, logo } from "./Header.css";
 
 export const Header = () => {
-	const { toggleTheme } = useThemeSwitcher();
+	const { theme, setTheme, toggleTheme } = useThemeSwitcher();
 
-	const navigateToSingularity = () => {
-		document.location.replace("/");
+	const resetTheme = () => {
+		// When using Astro view transitions, the theme classes applied to the
+		// document element get reset. This side effect re-applies the theme
+		// classes when the header mounts again.
+		if (theme !== "unset") {
+			setTheme(theme);
+		}
 	};
+
+	useEffect(() => {
+		document.addEventListener("astro:page-load", resetTheme);
+
+		return () => {
+			document.removeEventListener("astro:page-load", resetTheme);
+		};
+	}, []);
 
 	return (
 		<header className={header}>
 			<div className={container}>
-				<LogoButton className={logo} onClick={navigateToSingularity} />
+				<LogoButton className={logo} />
 				<ChibiButton className={chibi} onClick={toggleTheme} />
 			</div>
 
