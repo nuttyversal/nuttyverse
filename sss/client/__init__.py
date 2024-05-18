19ebe1a467e640a5439cc2aca54807609e9fee4b
nuttyversal <mail@nuttyver.se>
2024-05-17 23:04:30 -0700

    Connect to PostgreSQL database

--- sss/client/__init__.py
+++ sss/client/__init__.py
@@ -2,6 +2,7 @@ import io
 import os
 
 from minio import Minio
+import psycopg2
 
 
 class SpaceshipStorage:
@@ -25,7 +26,35 @@ class SpaceshipStorage:
 			secret_key=os.environ["MINIO_SECRET_KEY"],
 		)
 
+		if os.environ.get("DATABASE_NAME", None) is None:
+			raise MissingConfigError("Database name not specified.")
+
+		if os.environ.get("DATABASE_USER", None) is None:
+			raise MissingConfigError("Database user not specified.")
+
+		if os.environ.get("DATABASE_PASSWORD", None) is None:
+			raise MissingConfigError("Database password not specified.")
+
+		if os.environ.get("DATABASE_HOST", None) is None:
+			raise MissingConfigError("Database host not specified.")
+
+		if os.environ.get("DATABASE_PORT", None) is None:
+			raise MissingConfigError("Database port not specified.")
+
+		self.connection = psycopg2.connect(
+			dbname=os.environ["DATABASE_NAME"],
+			user=os.environ["DATABASE_USER"],
+			password=os.environ["DATABASE_PASSWORD"],
+			host=os.environ["DATABASE_HOST"],
+			port=os.environ["DATABASE_PORT"],
+		)
+
 	def store_object(self, bucket_name: str, object_name: str, data: io.BytesIO):
+		with self.connection.cursor() as cursor:
+			cursor.execute("SELECT version()")
+			single_row = cursor.fetchone()
+			print(f"version: {single_row}")
+
 		self.client.put_object(
 			bucket_name=bucket_name,
 			object_name=object_name,
