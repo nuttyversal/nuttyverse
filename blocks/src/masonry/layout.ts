17ce8c46a61b5d4a4971c50e37e892aa607d5e9e
nuttyversal <mail@nuttyver.se>
2024-05-20 18:22:49 -0700

    Implement masonry layout algorithm

--- blocks/src/masonry/layout.ts
+++ blocks/src/masonry/layout.ts
@@ -76,20 +76,58 @@ export type MasonryLayoutOutput = {
 export function layoutContentBlocks(
 	input: MasonryLayoutInput,
 ): MasonryLayoutOutput {
-	const origin: Position = {
-		x: 0,
-		y: 0,
-	};
+	// Keep track of the y-end for each column. The content blocks are laid out
+	// in an imperative fashion. A cursor is updated whenever a new content
+	// block is inserted into the cursor's column.
+	const columnCursors = Array.from({ length: input.columnCount }).map(() => 0);
 
-	const contentBlocksWithPosition = input.contentBlocks.map((block) => ({
-		...block,
-		...origin,
-	}));
+	// Compute the width of the Masonry content container without padding.
+	const totalPaddingX = (input.columnCount - 1) * input.paddingSize;
+	const usableContainerWidth = input.contentContainerWidth - totalPaddingX;
+	const singleColumnWidth = usableContainerWidth / input.columnCount;
+
+	// As the content blocks get laid out, they will have their relative
+	// positions within the Masonry content container annotated.
+	const annotatedContentBlocks: (ContentBlock & Position)[] = [];
+
+	for (const contentBlock of input.contentBlocks) {
+		// Figure out which column to insert the next content block into.
+		let columnIndex: number = -1;
+		let lowestCursorPosition = Infinity;
+
+		for (let index = 0; index < columnCursors.length; index++) {
+			if (columnCursors[index] < lowestCursorPosition) {
+				lowestCursorPosition = columnCursors[index];
+				columnIndex = index;
+			}
+		}
+
+		// Scale the content block to match the column width.
+		const resizedContentBlock: ContentBlock = {
+			width: singleColumnWidth,
+			height: (singleColumnWidth / contentBlock.width) * contentBlock.height,
+		};
+
+		// Annotate the position of the next content block.
+		annotatedContentBlocks.push({
+			...contentBlock,
+			x: columnIndex * singleColumnWidth + columnIndex * input.paddingSize,
+			y: lowestCursorPosition,
+		});
+
+		// Update the column cursor position.
+		columnCursors[columnIndex] +=
+			resizedContentBlock.height + input.paddingSize;
+	}
+
+	// Compute the total content container height.
+	const computedContainerHeight =
+		Math.max(...columnCursors) - input.paddingSize;
 
 	return {
-		contentBlocks: contentBlocksWithPosition,
+		contentBlocks: annotatedContentBlocks,
 		contentContainerWidth: input.contentContainerWidth,
-		contentContainerHeight: 0,
+		contentContainerHeight: computedContainerHeight,
 		columnCount: input.columnCount,
 		paddingSize: input.paddingSize,
 	};
