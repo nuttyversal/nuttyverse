2aca4e6581324db5bb3824a7efea95b80bf3664b
nuttyversal <mail@nuttyver.se>
2024-05-22 17:13:00 -0700

    Add fade-in animation

--- blocks/src/masonry/Masonry.css.ts
+++ blocks/src/masonry/Masonry.css.ts
@@ -1,10 +1,20 @@
-import { style } from "@vanilla-extract/css";
+import { keyframes, style } from "@vanilla-extract/css";
 
 export const contentContainer = style({
 	position: "relative",
 	width: "100%",
 });
 
+const fadeIn = keyframes({
+	from: {
+		opacity: 0,
+	},
+	to: {
+		opacity: 1,
+	},
+});
+
 export const contentBlock = style({
 	position: "absolute",
+	animation: `${fadeIn} 200ms ease-in`,
 });
