9b7809d2191ece0dd91fb035eb42a32d3a433bb2
nuttyversal <mail@nuttyver.se>
2024-05-26 16:31:50 -0700

    Implement progressive video loading

--- blocks/src/atoms/Video/Video.css.ts
+++ blocks/src/atoms/Video/Video.css.ts
@@ -1,9 +1,24 @@
 import { style } from "@vanilla-extract/css";
 import { colors } from "~/styles/themes/contract.css";
 
-export const base = style({
+export const container = style({
 	borderColor: colors.gray.solid["12"],
 	borderWidth: "3px",
 	borderRadius: "3px",
 	borderStyle: "solid",
+});
+
+export const loadingContainer = style({
+	borderColor: colors.gray.solid["11"],
+	transition: "all 300ms ease-out",
+	opacity: 0.2,
+});
+
+export const video = style({
+	filter: "none",
+	transition: "filter 500ms ease-out",
+});
+
+export const withGaussianBlur = style({
+	filter: "blur(16px)",
 });
--- blocks/src/atoms/Video/Video.tsx
+++ blocks/src/atoms/Video/Video.tsx
@@ -1,7 +1,52 @@
-import { base } from "./Video.css";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import {
+	container,
+	loadingContainer,
+	video,
+	withGaussianBlur,
+} from "./Video.css";
 
-type VideoProps = React.ComponentPropsWithoutRef<"video">;
+type VideoProps = {
+	/**
+	 * The URL of the video to display.
+	 */
+	src: string;
+
+	/**
+	 * The URL of the video to display as a preview video while the full-sized
+	 * video is loading.
+	 */
+	previewSrc?: string;
+} & React.ComponentPropsWithoutRef<"video">;
 
 export const Video = (props: VideoProps) => {
-	return <video className={base} {...props} />;
+	const { src, previewSrc, ...htmlVideoProps } = props;
+
+	const [videoSrc, setVideoSrc] = useState(previewSrc ? previewSrc : src);
+
+	useEffect(() => {
+		if (props.previewSrc) {
+			const video = document.createElement("video");
+			video.src = props.src;
+
+			video.onloadeddata = () => {
+				setVideoSrc(props.src);
+			};
+		}
+	}, []);
+
+	const videoClassNames = classNames(video, {
+		[withGaussianBlur]: videoSrc === previewSrc,
+	});
+
+	return (
+		<div className={videoSrc === previewSrc ? loadingContainer : container}>
+			<video
+				src={videoSrc}
+				className={videoClassNames}
+				{...htmlVideoProps}
+			/>
+		</div>
+	);
 };
