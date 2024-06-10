eb66b78c137f7752c663be9a2342fe0de84cd3e6
nuttyversal <mail@nuttyver.se>
2024-06-10 00:22:35 -0700

    Pin Neogit to v0.0.1
    This is the last version that has full support for Neovim 0.9. After
    this point, Neovim 0.10 APIs were introduced. Currently waiting for
    Nixpkgs to update to Neovim 0.10.

--- tools/nvim/lua/plugins/neogit.lua
+++ tools/nvim/lua/plugins/neogit.lua
@@ -5,4 +5,8 @@ return {
 		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
+	-- [TODO] Pin to the last version that has full support for Neovim 0.9.
+	-- After this point, Neovim 0.10 APIs were introduced. Currently waiting
+	-- for Nixpkgs to update to Neovim 0.10.
+	version = "v0.0.1",
 }
