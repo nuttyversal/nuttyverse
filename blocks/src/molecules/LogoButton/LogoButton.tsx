c5cf6df22f663f7efcb9cf55961991b580189c2e
nuttyversal <mail@nuttyver.se>
2024-06-13 12:40:22 -0700

    Add tooltip to `LogoButton` component

--- blocks/src/molecules/LogoButton/LogoButton.tsx
+++ blocks/src/molecules/LogoButton/LogoButton.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
-import { Logo } from "~/atoms/Logo/Logo";
+import { Logo } from "~/atoms/Logo";
+import { Text } from "~/atoms/Text";
+import { Tooltip } from "~/atoms/Tooltip";
 import { base } from "./LogoButton.css";
 
 type LogoButtonProps = {
@@ -8,14 +10,31 @@ type LogoButtonProps = {
 };
 
 export const LogoButton: React.FC<LogoButtonProps> = (props) => {
+	const tooltipContent = {
+		type: "element",
+		element: (
+			<>
+				<Text as="span" size="smol" marginless>
+					Navigate to the{" "}
+				</Text>
+
+				<Text as="span" size="smol" marginless weight={700} wdth={100}>
+					Singularity
+				</Text>
+			</>
+		),
+	} as const;
+
 	return (
 		<a
 			href="/"
 			onClick={props.onClick}
-			aria-label="Navigate to Singularity"
+			aria-label="Navigate to the Singularity"
 			className={classNames(props.className, base)}
 		>
-			<Logo className={props.className} />
+			<Tooltip content={tooltipContent}>
+				<Logo className={props.className} />
+			</Tooltip>
 		</a>
 	);
 };
