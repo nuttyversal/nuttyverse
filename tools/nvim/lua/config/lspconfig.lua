1add2bba593ef6c3520bd39a16f840e42f91fd3e
nuttyversal <mail@nuttyver.se>
2024-07-03 16:01:40 -0700

    Use Telescope for LSP mappings

--- tools/nvim/lua/config/lspconfig.lua
+++ tools/nvim/lua/config/lspconfig.lua
@@ -13,22 +13,39 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
 	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+
+	vim.keymap.set("n", "gd", function()
+		require("telescope.builtin").lsp_definitions()
+	end, bufopts)
+
+	vim.keymap.set("n", "gi", function()
+		require("telescope.builtin").lsp_implementations()
+	end, bufopts)
+
+	vim.keymap.set("n", "gr", function()
+		require("telescope.builtin").lsp_references()
+	end, bufopts)
+
+	vim.keymap.set("n", "ge", function()
+		require("telescope.builtin").diagnostics({ bufnr = 0 })
+	end, bufopts)
+
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+
 	vim.keymap.set("n", "<space>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
+
 	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+
 	vim.keymap.set("n", "<space>ff", function()
 		vim.lsp.buf.format({ async = true })
 	end, bufopts)
