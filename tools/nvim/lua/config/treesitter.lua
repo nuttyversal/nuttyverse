9d3419496113ccedaf6d107c73c04a0548bbc72b
nuttyversal <mail@nuttyver.se>
2024-07-03 13:48:53 -0700

    Highlight syntax for justfiles
    ✦ Add autocommand to detect justfiles and set filetype accordingly.
    ✦ Add just to the list of treesitter parsers to install.

--- tools/nvim/lua/config/treesitter.lua
+++ tools/nvim/lua/config/treesitter.lua
@@ -1,8 +1,16 @@
 local treesitter = require("nvim-treesitter.configs")
 
+vim.cmd([[
+	augroup filetypedetect
+		autocmd! BufRead,BufNewFile justfile setfiletype just
+	augroup END
+]])
+
 treesitter.setup({
 	modules = {},
-	ensure_installed = {},
+	ensure_installed = {
+		"just",
+	},
 	ignore_install = {},
 	sync_install = false,
 	auto_install = true,
