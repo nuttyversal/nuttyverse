23a75b1404ad3839eac4c36b2d91226c3ec50480
nuttyversal <mail@nuttyver.se>
2024-07-03 21:22:00 -0700

    Install Astro plugin for Prettier

--- city/prettier.config.cjs
+++ city/prettier.config.cjs
@@ -2,4 +2,13 @@ module.exports = {
 	tabWidth: 3,
 	arrowParens: "always",
 	trailingComma: "all",
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
