4d6f1b975571e07bc2a3c8d8e7d0896b65c43cfb
nuttyversal <mail@nuttyver.se>
2024-07-07 18:26:07 -0700

    Add tests for `Heading` component

---
+++ blocks/src/atoms/Heading/Heading.spec.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Heading } from "./Heading";
+
+const headingContent = "Heading";
+
+describe("Heading", () => {
+	it("should render the heading content with the correct level", () => {
+		const levels = ["h2", "h3", "h4", "h5", "h6"] as const;
+
+		levels.forEach((h) => {
+			// Arrange
+			render(<Heading type={h}>{headingContent}</Heading>);
+
+			// Assert
+			const headings = document.getElementsByTagName(h);
+			expect(headings).toHaveLength(1);
+			expect(headings[0]).toHaveTextContent(headingContent);
+		});
+	});
+
+	it("should throw an error if the type is 'h1'", () => {
+		// Arrange
+		const renderError = () => {
+			const h1 = "h1" as ComponentProps<typeof Heading>["type"];
+			render(<Heading type={h1}>{headingContent}</Heading>);
+		};
+
+		// Assert
+		expect(renderError).toThrow();
+	});
+});
