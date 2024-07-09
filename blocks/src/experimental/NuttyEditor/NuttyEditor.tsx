4ef1090da29026bc856e82e4718179191c070bd0
nuttyversal <mail@nuttyver.se>
2024-07-09 15:32:58 -0700

    Compile Markdown content into React component

--- blocks/src/experimental/NuttyEditor/NuttyEditor.tsx
+++ blocks/src/experimental/NuttyEditor/NuttyEditor.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import * as runtime from "react/jsx-runtime";
 import { basicSetup } from "codemirror";
 import { indentUnit } from "@codemirror/language";
 import { EditorState } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
 import { indentWithTab } from "@codemirror/commands";
+import { evaluate } from "@mdx-js/mdx";
 import { vim } from "@replit/codemirror-vim";
 import { editorContainer } from "./NuttyEditor.css";
+import { MDXContent } from "mdx/types";
 
 export const NuttyEditor: React.FC = () => {
 	const editorContainerRef = useRef<HTMLDivElement>(null);
 	const editorViewRef = useRef<EditorView | null>(null);
 	const [documentText, setDocumentText] = useState<string>("");
+	const mdxContent = useRef<MDXContent | null>(null);
+	const Content = mdxContent.current ?? Fragment;
 
 	useEffect(() => {
 		if (editorContainerRef.current) {
@@ -32,9 +37,19 @@ export const NuttyEditor: React.FC = () => {
 					keymap.of([indentWithTab]),
 
 					// Listen to state changes.
-					EditorView.updateListener.of((update) => {
+					EditorView.updateListener.of(async (update) => {
 						if (update.docChanged) {
+							// Store the document text.
 							setDocumentText(update.state.doc.toString());
+
+							// Compile and evaluate the document text into a functional component.
+							const { default: MdxContent } = await evaluate(
+								update.state.doc.toString(),
+								// @ts-expect-error: `runtime` types are broken.
+								runtime,
+							);
+
+							mdxContent.current = MdxContent;
 						}
 					}),
 				],
@@ -48,9 +63,10 @@ export const NuttyEditor: React.FC = () => {
 	}, []);
 
 	return (
-		<>
+		<div style={{ display: "flex", flexDirection: "column", gap: "1em" }}>
 			<div ref={editorContainerRef} className={editorContainer} />
-			<pre>{documentText}</pre>
-		</>
+			<hr />
+			<Content />
+		</div>
 	);
 };
--- blocks/vite.config.mts
+++ blocks/vite.config.mts
@@ -1,3 +1,4 @@
+import mdx from "@mdx-js/rollup";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
@@ -5,5 +6,5 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), vanillaExtractPlugin(), tsconfigPaths()],
+	plugins: [react(), vanillaExtractPlugin(), mdx(), tsconfigPaths()],
 });
