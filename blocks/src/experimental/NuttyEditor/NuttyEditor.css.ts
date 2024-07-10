e3bb469ab0b485219dd6d3f4b1457eb65011eb1b
nuttyversal <mail@nuttyver.se>
2024-07-09 17:12:51 -0700

    Add border to the editor container

--- blocks/src/experimental/NuttyEditor/NuttyEditor.css.ts
+++ blocks/src/experimental/NuttyEditor/NuttyEditor.css.ts
@@ -1,6 +1,13 @@
 import { globalStyle, style } from "@vanilla-extract/css";
+import { colors } from "~/styles/themes/contract.css";
+import { spacing } from "~/styles/tokens/spacing";
 
-export const editorContainer = style({});
+export const editorContainer = style({
+	borderWidth: spacing[1],
+	borderStyle: "solid",
+	borderColor: colors.gray.solid["12"],
+	borderRadius: spacing[1],
+});
 
 globalStyle(`${editorContainer} .cm-editor *`, {
 	fontFamily: "PragmataPro Mono Liga !important",
