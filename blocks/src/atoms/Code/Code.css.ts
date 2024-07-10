fe7fa09d722e7f2fde7af14063084f065d70452a
nuttyversal <mail@nuttyver.se>
2024-07-10 13:11:33 -0700

    Tweak `Code` styling
    ✦ Set font weight to bold.
    ✦ Reduce font size to smol.
    ✦ Add padding to top and bottom.
    ✦ Clone box decoration over line breaks to prevent slicing.

--- blocks/src/atoms/Code/Code.css.ts
+++ blocks/src/atoms/Code/Code.css.ts
@@ -1,14 +1,25 @@
 import { style } from "@vanilla-extract/css";
 import { colors } from "~/styles/themes/contract.css";
 import { spacing } from "~/styles/tokens/spacing";
+import { experimentalTypeScale } from "~/styles/tokens/typography";
 
 export const inlineCode = style({
 	fontFamily: "PragmataPro Mono Liga",
+	fontSize: experimentalTypeScale.smol,
+	fontWeight: "bold",
+
 	marginLeft: spacing[0.5],
 	marginRight: spacing[0.5],
+	paddingTop: spacing[0.5],
+	paddingBottom: spacing[0.5],
 	paddingLeft: spacing[1],
 	paddingRight: spacing[1],
 	borderRadius: spacing[1],
+
+	// Prevent slicing over line breaks.
+	boxDecorationBreak: "clone",
+	WebkitBoxDecorationBreak: "clone",
+
 	backgroundColor: colors.gray.solid["11"],
 	color: colors.gray.solid["01"],
 });
