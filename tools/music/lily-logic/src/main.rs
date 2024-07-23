14331b08d0ed5860daf94d0397d40736260c072a
nuttyversal <mail@nuttyver.se>
2024-07-23 10:30:58 -0700

    Add models for note events from Logic Pro

--- tools/music/lily-logic/src/main.rs
+++ tools/music/lily-logic/src/main.rs
@@ -1,3 +1,42 @@
+#[derive(Debug, Clone, Copy)]
+struct TimeSignature {
+	numerator: u8,
+	denominator: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Time {
+	bar: u16,
+	beat: u16,
+	division: u16,
+	ticks: u16,
+}
+
+#[derive(Debug)]
+struct Event {
+	position: Time,
+	length: Time,
+}
+
 fn main() {
-	println!("Hello, world!");
+	let time_signature = TimeSignature {
+		numerator: 4,
+		denominator: 4,
+	};
+
+	let time = Time {
+		bar: 1,
+		beat: 1,
+		division: 1,
+		ticks: 1,
+	};
+
+	let event = Event {
+		position: time,
+		length: time,
+	};
+
+	println!("{:?}", time_signature);
+	println!("{:?}", time);
+	println!("{:?}", event);
 }
