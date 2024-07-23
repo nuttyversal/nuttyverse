980ce8e669d30d901b2b555ec32a1cf9aed8c7f7
nuttyversal <mail@nuttyver.se>
2024-07-23 10:50:38 -0700

    Annotate struct fields with descriptions

--- tools/music/lily-logic/src/main.rs
+++ tools/music/lily-logic/src/main.rs
@@ -1,20 +1,33 @@
 #[derive(Debug, Clone, Copy)]
 struct TimeSignature {
+	/// The number of beats in a bar.
 	numerator: u8,
+
+	/// The note value that represents one beat.
 	denominator: u8,
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Time {
+	/// The bar number (one-based indexing).
 	bar: u16,
+
+	/// The beat number (one-based indexing).
 	beat: u16,
+
+	/// The division number (one-based indexing).
 	division: u16,
+
+	/// The tick number (one-based indexing).
 	ticks: u16,
 }
 
 #[derive(Debug)]
 struct Event {
+	/// The position of the event in the sequence.
 	position: Time,
+
+	/// The length of the event in the sequence.
 	length: Time,
 }
 
