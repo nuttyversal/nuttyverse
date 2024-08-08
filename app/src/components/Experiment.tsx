8f8ecf3decbeb7592797950e795a6ec571fd6e32
nuttyversal <mail@nuttyver.se>
2024-08-08 11:46:01 -0700

    Tweak `Header` component styles

--- app/src/components/Experiment.tsx
+++ app/src/components/Experiment.tsx
@@ -8,30 +8,20 @@ const Experiment = () => {
 			<div class={styles.content}>
 				<Header />
 
-				<p class="with-drop-cap">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-					imperdiet vitae lorem vitae malesuada. Maecenas id nisi risus.
-					Morbi sed massa mattis, pulvinar orci quis, posuere quam. Etiam
-					efficitur faucibus lacus aliquam sagittis. Vivamus quis augue
-					vitae augue consectetur mollis. Ut sed elementum nisl.
-				</p>
+				<div class={styles.main}>
+					<p class="with-drop-cap">
+						Ahoy there, fellow navigator! Welcome to my internet expanse.
+						You might notice a seeming emptiness, but do not fret. You've
+						simply arrived early enough to witness the expansion of this
+						universe.
+					</p>
 
-				<p>
-					Sed mollis ac tortor eget dignissim. Nunc vitae gravida mauris,
-					in suscipit libero. Duis porttitor mauris in enim pulvinar
-					finibus. Sed viverra vel arcu et ornare. Suspendisse vitae
-					pharetra dui. Praesent scelerisque, elit quis vestibulum
-					sollicitudin, mauris nunc posuere libero, accumsan maximus nunc
-					quam eu urna. Mauris eget urna lacus.
-				</p>
-
-				<p>
-					Visit the{" "}
-					<Link href="https://nuttyver.se" newTab>
-						Nuttyverse
-					</Link>{" "}
-					website.
-				</p>
+					<p>
+						Just as the physical universe materialized from the Big Bang,
+						this website will similarly unfold from this point. Emerging
+						from this singularity will bloom a new digital universe! 😀
+					</p>
+				</div>
 			</div>
 		</div>
 	);
