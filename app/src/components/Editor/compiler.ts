b071597904f574a8ca730a75d50c5afd97d679d4
nuttyversal <mail@nuttyver.se>
2024-08-12 16:07:43 -0700

    Create remark plugin to rewrite links

--- app/src/components/Editor/compiler.ts
+++ app/src/components/Editor/compiler.ts
@@ -5,6 +5,18 @@ import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { JSX } from "solid-js";
 import * as runtime from "solid-js/h/jsx-runtime";
+import { Plugin } from "unified";
+import { Node } from "unist";
+import { visit } from "unist-util-visit";
+import { Link } from "~/components/Link";
+
+/**
+ * A component registry that maps component names to components.
+ * Components must be registered here to be used in MDX content.
+ */
+const componentRegistry = {
+	Link,
+};
 
 /**
  * An effect that compiles MDX content into JSX.
@@ -16,17 +28,58 @@ const compileMdx = (
 		try {
 			const { default: renderContent } = await evaluate(mdx, {
 				...runtime,
-				rehypePlugins: [rehypeKatex],
+				rehypePlugins: [rehypeKatex, rewriteLinks],
 				remarkPlugins: [remarkMath],
 			});
 
 			return renderContent({
-				components: {},
+				components: componentRegistry,
 			});
 		} catch (e) {
 			return Effect.fail(new Error(`Failed to compile MDX: ${e}`));
 		}
 	});
+};
+
+/**
+ * Rewrite `<a>` elements into `Link` component instances.
+ */
+const rewriteLinks: Plugin = () => {
+	// Inferred from the console output.
+	// Does this type exist somewhere?
+	type RemarkElement = {
+		tagName: string;
+		properties?: Record<string, unknown>;
+		children: Node[];
+	};
+
+	return (tree) => {
+		visit(
+			tree,
+			"element",
+			(node: RemarkElement, index, parent: RemarkElement) => {
+				if (node.tagName === "a") {
+					const href = node.properties?.href as string;
+
+					if (href) {
+						const component = {
+							type: "element",
+							tagName: "Link",
+							children: node.children,
+							properties: {
+								href,
+								newTab: !href.startsWith("/"),
+							},
+						};
+
+						if (parent) {
+							parent.children[index] = component;
+						}
+					}
+				}
+			},
+		);
+	};
 };
 
 export { compileMdx };
