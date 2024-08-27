e90645ba4bdc7a3e0d7ea79b6d9436c9dd751d21
nuttyversal <mail@nuttyver.se>
2024-08-27 13:18:28 -0700

    Customize Neovide cursor effects

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -94,6 +94,11 @@ vim.opt.fillchars = {
 if vim.g.neovide then
 	vim.g.neovide_cursor_antialiasing = true
 	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.g.neovide_cursor_vfx_particle_lifetime = 5.0
+	vim.g.neovide_cursor_vfx_particle_density = 50.0
+	vim.g.neovide_cursor_vfx_particle_speed = 20.0
+	vim.g.neovide_cursor_animation_length = 0.05
+	vim.g.neovide_cursor_trail_size = 0.8
 	vim.g.neovide_refresh_rate = 144
 	vim.g.neovide_scale_factor = 1.0
 
